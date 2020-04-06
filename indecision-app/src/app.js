e12d28bebb5e822526fadbc4580753fa33462b2a
SteeshMcK <stesha.norstrom@pcc.edu>
2020-04-06 14:54:16 -0700

    install validator, react, and react-dom

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,8 +1,20 @@
-import {square, add} from './utils.js';
-import {isAdult, canDrink} from './person.js';
+// import subtract, {square, add} from './utils.js';
+// import isSenior, {isAdult, canDrink} from './person.js';
+// import validator from 'validator';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 console.log('app.js is running!!');
-console.log(square(10));
-console.log(add(35, 45));
-console.log(isAdult(51));
-console.log(canDrink(51));
+
+// console.log(square(10));
+// console.log(add(35, 45));
+// console.log(subtract(451, 65));
+
+// console.log(isAdult(51));
+// console.log(canDrink(51));
+// console.log(isSenior(51));
+
+// console.log(validator.isEmail('cleodecarabas@yahoo.com'));
+
+const template = React.createElement('p', {}, 'Testing one, two, three...');
+ReactDOM.render(template, document.getElementById('app'));
--- indecision-app/src/utils.js
+++ indecision-app/src/utils.js
@@ -4,5 +4,8 @@ const square = (x) => x * x;
 
 const add = (a, b) => a + b;
 
-export {square, add};
+const subtract = (a, b) => a - b;
+
+export {square, add, subtract as default};
+
 // exports - default export - named exports
